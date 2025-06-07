2bc2453f002fe080f9b77386f066e928b85d5cf8
alextalijan <acatalijan@icloud.com>
2025-06-07 15:08:19 -0500

    style: add flex layout for project cards

---
+++ src/js/display-controller.js
@@ -0,0 +1,9 @@
+const displayController = function () {
+    const render = function (projects) {
+        for (const project of projects) {
+            
+        }
+    };
+
+    return { render };
+};
