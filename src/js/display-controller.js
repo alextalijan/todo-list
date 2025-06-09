5df84b55e88dd803936217b63eefb21e34316847
alextalijan <acatalijan@icloud.com>
2025-06-09 13:07:07 -0500

    feat: write logic for rendering projects on screen
    Display controller loops through all of the projects given to it, and
    renders them as cards on the page.

--- src/js/display-controller.js
+++ src/js/display-controller.js
@@ -1,7 +1,27 @@
-const displayController = function () {
+export const displayController = function () {
     const render = function (projects) {
+        const projectsDiv = document.querySelector("#projects");
+
         for (const project of projects) {
-            
+            const projectDiv = document.createElement("div");
+            projectDiv.className = "project-card";
+
+            const heading = document.createElement("h2");
+            heading.textContent = project.title;
+            heading.classList.add("project-title");
+
+            const description = document.createElement("p");
+            description.textContent = project.description;
+            description.classList.add("project-description");
+
+            const dueDate = document.createElement("p");
+            dueDate.textContent = `Due Date: ${project.dueDate}`;
+            dueDate.classList.add("project-due-date");
+
+            projectDiv.appendChild(heading);
+            projectDiv.appendChild(description);
+            projectDiv.appendChild(dueDate);
+            projectsDiv.appendChild(projectDiv);
         }
     };
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 import "../css/styles.css";
 import { Project } from "./project-class.js";
+import { displayController } from "./display-controller.js";
 
 const myProjects = [];
+
+displayController.render(myProjects);
